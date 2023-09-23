05198f5dc9f568e70080149d329165418a3c6cf3
sfuller <sam@samfullerstudios.com>
2023-09-22 23:57:48 -0700

    Add location to sampler uniform in imgui.frag.glsl

--- CeresGpuDearImgui/Backend/imgui.frag.glsl
+++ CeresGpuDearImgui/Backend/imgui.frag.glsl
@@ -3,7 +3,7 @@
 layout(location = 0) in vec2 Frag_UV;
 layout(location = 1) in vec4 Frag_Color;
 
-layout(binding = 1) uniform sampler2D Texture;
+layout(location = 0, binding = 1) uniform sampler2D Texture;
 
 layout (location = 0) out vec4 Out_Color;
 
